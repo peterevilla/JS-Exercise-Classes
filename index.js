157a95f1bb667e50f5101c7ef7c4609a158c6c03
IT2 <it2@AWT.local>
2019-12-12 13:56:37 -0600

    MVP

--- index.js
+++ index.js
@@ -226,7 +226,24 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+
+    constructor ({name: name, age: age, location: location, specialty: specialty, favLanguage: favLanguage, catchPhrase: catchPhrase, gradClassName: gradClassName, favInstructor: favInstructor}) {
+      super({name: name, age: age, location: location, specialty: specialty, favLanguage: favLanguage, catchPhrase: catchPhrase});
+
+      this.gradClassName = gradClassName;
+      this.favInstructor = favInstructor
+    }
+
+    standUp(channel) {
+
+        return `${this.name} announces to ${channel}, @channel standy times!`
+    }
+
+    debugsCode(student, subject) {
+
+        return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
 
 }
 
